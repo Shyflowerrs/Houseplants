c58394ca44f67bfa046bae11ed382ccbb9d1cab3
Shyflowerrs <chelseah@live.ca>
2022-11-24 19:09:00 -0600

    Added about page to active admin dashboard

--- db/migrate/20221125004406_create_abouts.rb
+++ db/migrate/20221125004406_create_abouts.rb
@@ -1,7 +1,8 @@
 class CreateAbouts < ActiveRecord::Migration[7.0]
   def change
     create_table :abouts do |t|
-
+      t.string :title
+      t.string :content
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_221702) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_004406) do
+  create_table "abouts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
